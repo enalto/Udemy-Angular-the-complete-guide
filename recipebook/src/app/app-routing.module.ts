4c2fe3bf7cf74abcd3ff88b56b4d876958365a9f
enalto <enaltogondrige@gmail.com>
2022-04-29 13:17:58 -0300

    Adding RoutingModule

--- recipebook/src/app/app-routing.module.ts
+++ recipebook/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'shoppingList', component: ShoppingListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
